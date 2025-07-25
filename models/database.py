3f4710ae9dcc68f70abe611b8f2dea6d0021249d
Dharshan C S <23f3004028@ds.study.iitm.ac.in>
2025-07-25 16:21:23 +0530

    Database creation and configuration

--- models/database.py
+++ models/database.py
@@ -1,13 +1,57 @@
 import sqlite3
 
 def create_database():
+
     conn = sqlite3.connect("parking_db")
+    conn.execute("PRAGMA foreign_keys = ON")  # Enable foreign keys
     curr = conn.cursor()
-    curr.execute('''CREATE TABLE IF NOT EXISTS USERS
-                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    USERNAME TEXT NOT NULL)''')
+
+
+    curr.execute('''
+                CREATE TABLE IF NOT EXISTS USERS(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE,
+                address TEXT,
+                pincode INTEGER,
+                password TEXT NOT NULL
+                );
+                ''')
+    curr.execute('''
+                CREATE TABLE IF NOT EXISTS ADMIN(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL               
+                );
+                ''')
+    curr.execute('''
+                CREATE  TABLE IF NOT EXISTS PARKING_LOT(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                prime_location TEXT NOT NULL,
+                price INTEGER DEFAULT '250',
+                address TEXT NOT NULL,
+                pincode INTEGER NOT NULL,
+                max_no_of_spots INTEGER NOT NULL DEFAULT '10'
+                );
+                ''')
+    curr.execute('''CREATE TABLE IF NOT EXISTS PARKING_SPOT(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                lot_id INTEGER NOT NULL,
+                slot_number TEXT NOT NULL,
+                status TEXT DEFAULT 'A',
+                FOREIGN KEY(lot_id) REFERENCES PARKING_LOT(id) ON DELETE CASCADE
+                ); 
+                ''') 
+    curr.execute('''CREATE TABLE IF NOT EXISTS booking_details(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                slot_id INTEGER NOT NULL,
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY(user_id) REFERENCES USERS(id),
+                FOREIGN KEY(slot_id) REFERENCES PARKING_SPOT(id)
+                );
+                ''')
     conn.commit()
     conn.close()
-    print("Done")
+    print("Database created succesfully")
 
-create_database()
