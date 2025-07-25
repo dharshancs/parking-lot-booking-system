2038508d123006f1bcf0684aacc1ec3a5751b0d8
Dharshan C S <23f3004028@ds.study.iitm.ac.in>
2025-07-26 03:36:52 +0530

    creates database from database.py and adds admin credentials to table

--- models/config.py
+++ models/config.py
@@ -1,5 +1,5 @@
 import sqlite3
-from database import create_database
+from .database import create_database   #If i dont use .database python will look from working database and not relative
 
 def initialize_table():
     create_database()
@@ -12,7 +12,7 @@ def initialize_table():
 
     curr.execute('''INSERT OR IGNORE INTO ADMIN(username,password) VALUES(?,?);''',(admin_username,admin_password)) #insert admin cred into table and ignore if already exists
 
-    conn.execute()
+    conn.commit()
     conn.close()
 
-    print("Admin credentials added into tabke /n")
+    #print("Admin credentials added into tabke /n")
