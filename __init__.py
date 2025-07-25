1090f173a180b8a2c8afa645802bcbbe2f16a0f9
Dharshan C S <23f3004028@ds.study.iitm.ac.in>
2025-07-26 04:45:15 +0530

    Added blueprints

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 #app factory
 from flask import Flask
-from controllers.views import view
+from controllers.basic_views import b_view
+from controllers.user_views import u_view
 from models.config import initialize_table
 
 
@@ -8,6 +9,6 @@ def create_app():
     app = Flask(__name__)
 
     initialize_table()
-    app.register_blueprint(view)
-
+    app.register_blueprint(b_view)
+    app.register_blueprint(u_view)
     return app
