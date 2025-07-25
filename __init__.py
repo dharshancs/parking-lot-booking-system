aaffd76fef449760f4479f2dd963db93503dca53
Dharshan C S <23f3004028@ds.study.iitm.ac.in>
2025-07-26 03:39:24 +0530

    Added routes via blueprint

--- __init__.py
+++ __init__.py
@@ -1,7 +1,13 @@
 #app factory
 from flask import Flask
+from controllers.views import view
+from models.config import initialize_table
+
 
 def create_app():
     app = Flask(__name__)
+
+    initialize_table()
+    app.register_blueprint(view)
 
     return app
